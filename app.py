fecd29c5465df3799382251ba6f0a102de492805
Einthiri Mudili <einthirimudili@Einthiris-MacBook-Pro.local>
2020-07-09 19:17:17 -0500

    taha is a good person

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import jsonify
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/resume")
+def resume():
+    f = open("resume.json", "r")
+    return jsonify(f.read())
+
+# run the application
+if __name__ == "__main__":
+    app.run(debug=False)
